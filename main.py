9dc0bf416a02c817ea97a4f9781b2e4cbcaa3978
r-rhfouda <rimerh1904@gmail.com>
2026-04-29 10:34:55 +0100

    Add files via upload
    LE CODE SE TROUVE DANS main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-print('essai')
-url = input('enter the URL desired: ')
-
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+import time
+driver = webdriver.Chrome()
+url = "https://www.producthunt.com/search?q=mental+health+ai"
+driver.get(url)
+#TIME TO LOAD
+time.sleep(10)
+#CSS selector is complete
+elements = driver.find_elements(By.CSS_SELECTOR, 'a[data-test="post-name"]')
+app_names = []
+for item in elements:
+    app_names.append({"name": item.text})
+with open('products.json', 'w') as f:
+    json.dump(app_names, f, indent=4)
+driver.quit()
+print('done')
